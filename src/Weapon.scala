4fd73e076c2fe8c981cefbec58c6e2738ee74dbe
Caroline Hart <cmhart2@buffalo.edu>
2019-02-15 13:27:01 -0500

    I added a player and weapon class

---
+++ src/Weapon.scala
@@ -0,0 +1,8 @@
+// Version 1: 2/15/2019
+// Author: Caroline Hart
+
+class Weapon(var name: String, var damage: Int) {
+  /** A weapon object can be obtained by players to use in the game
+    * Players can use weapons to damage the health of their competitors
+    */
+}
