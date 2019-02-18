c92af9af4aada197d67b79ec35e3c69f548e678f
Caroline Hart <cmhart2@buffalo.edu>
2019-02-18 16:28:46 -0500

    Added a BackEnd package, Play object, and updates to Player and Weapons classes

---
+++ src/BackEnd/Weapon.scala
@@ -0,0 +1,37 @@
+package BackEnd
+
+import scala.util.Random
+
+/**
+  * @author <Caroline Hart>
+  * @group <Alyssa Shellman, Elijah Scuderi, Caroline Hart>
+  * @version <2/15/2019>
+  * @todo <Some weapons need to be more rare than others>
+  */
+
+
+class Weapon(var name: String, var damage: Int) {
+  /**
+    * A weapon object can be obtained by players to use in the game
+    * Players can use weapons to damage the health of their competitors
+    */
+  def findWeapon(): Weapon = {
+    /**
+      * @return <A new Weapon object>
+      * @desc <This method randomly assigns a Weapon object>
+      */
+    val randomNum: Int = Random.nextInt(4)
+    if (randomNum == 0) {
+      new Weapon("Sword", 20)
+    }
+    else if (randomNum == 1) {
+      new Weapon("Gun", 40)
+    }
+    else if (randomNum == 2) {
+      new Weapon("Axe", 25)
+    }
+    else {
+      new Weapon("Baseball Bat", 10)
+    }
+  }
+}
--- src/Player.scala
+++
@@ -1,8 +0,0 @@
-// Version 1: 2/15/2019
-// Author: Caroline Hart
-
-class Player(var health: Int, var weapon: Weapon) {
-  /** Each player has health and weapon
-    *
-    */
-}
