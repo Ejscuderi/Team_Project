23423a7cae497ef11dd5df6b2b36d9959ead9eea
Alyssa <ajshellm@buffalo.edu>
2019-03-25 15:38:07 -0400

    .

--- src/BackEnd/PlayerStates/standing.scala
+++ src/BackEnd/PlayerStates/standing.scala
@@ -3,4 +3,8 @@ import BackEnd.Player
 
 class standing(var player: Player) extends playerState(player) {
 
+  override def wPress(): Unit = {}
+  override def aPress(): Unit = {}
+  override def sPress(): Unit = {}
+  override def dPress(): Unit = {}
 }
--- src/BackEnd/PlayerStates/walking.scala
+++ src/BackEnd/PlayerStates/walking.scala
@@ -1,5 +1,25 @@
 package BackEnd.PlayerStates
 
-class walking {
+import BackEnd.Player
 
+class walking(player: Player) extends playerState(player) {
+
+
+//  20 should be cell size
+
+  override def aPress(): Unit = {
+    player.position(0) = player.position(0) - 20
+  }
+
+  override def dPress(): Unit = {
+    player.position(0) = player.position(0) + 20
+  }
+
+  override def wPress(): Unit = {
+    player.position(1) = player.position(1) - 20
+  }
+
+  override def sPress(): Unit = {
+    player.position(1) = player.position(1) + 20
+  }
 }
