bf76ec215cc253391b94d078fb4cb3fd5885f56d
Caroline Hart <cmhart2@buffalo.edu>
2019-05-02 13:08:46 -0400

    Major changes to project

--- FrontEnd/getMaze.js
+++
@@ -1,6 +0,0 @@
-import Maze from 'src/BackEnd/PlayerStates'
-
-function loadMaze() {
-    var currentMaze = new Maze();
-    currentMaze.makeMaze1();
-}
--- src/FrontEnd/getMaze.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs");
-
-var data = "New File Contents";
-
-fs.writeFile("src/FrontEnd/temp.txt", data, function(err, data) {
-    if (err) console.log(err);
-    console.log("Successfully Written to File.");
-});
